cd7fa6e4651517bd9c68c6c70760e31d4f3c4899
Chavozoom <isaquechaves10500@gmail.com>
2023-03-06 23:27:23 -0300

    Final project

---
+++ Test/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try { 
+        List<int> sequence = new List<int> { 0, 1};
+        int number = int.Parse(Console.ReadLine());
+        int[] seq = new int[] {0,1 };
+
+        while(sequence.Max() < number)
+        {
+            int next;
+            next = seq[0] + seq[1];
+            sequence.Add(next);
+            seq[0] = seq[1];
+            seq[1] = next;
+        }
+        Console.WriteLine(sequence.Contains(number));
+
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
+
